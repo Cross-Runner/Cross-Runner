3db25c316a5f0fb286f7aafa437e7f243e873e37
edvardkennedy <edvardspillum07@gmail.com>
2025-11-14 11:17:52 +0100

    Skins section layout complete

--- cross-runner-main-gd/addons/scenemanager.gd/scenemanager.gd
+++ cross-runner-main-gd/addons/scenemanager.gd/scenemanager.gd
@@ -1,6 +1,4 @@
 @tool
 extends EditorPlugin
 
-
-
 var last_scene_path: String = ""
--- cross-runner-main-gd/button_.gd
+++ cross-runner-main-gd/button_.gd
@@ -4,5 +4,4 @@ func _ready() -> void:
 	connect("pressed", Callable(self, "_on_pressed"))
 
 func _on_pressed() -> void:
-	if SceneManager.last_scene_path != "":
-		get_tree().change_scene_to_file(SceneManager.last_scene_path)
+	get_tree().change_scene_to_file("res://Main Menu.tscn")
