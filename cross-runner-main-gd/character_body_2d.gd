77721b2b011738ca20b92b0e3951e6179c16f385
Gjilde <gildeoliver07@gmail.com>
2025-11-21 10:53:56 +0100

    12

--- cross-runner-main-gd/character_body_2d.gd
+++ cross-runner-main-gd/character_body_2d.gd
@@ -1,10 +1,5 @@
 extends CharacterBody2D
 
-<<<<<<< HEAD
-var health = 500
-var stunned = false
-var stun_timer = 0.0
-=======
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
@@ -21,27 +16,13 @@ var jumps_left = MAX_JUMPS
 
 # For å hindre at man "avbryter" attack med idle/walk
 var is_attacking = false
->>>>>>> 99cc24945278296f8c06a1e423dad4b6b1c984b0
 
 var coin_counter = 0
 
 func _physics_process(delta: float) -> void:
-	# 1. Stun Timer
-	if stunned:
-		stun_timer -= delta
-		if stun_timer <= 0:
-			stunned = false
-			print("Boss recovered!")
-
-	# 2. Gravity Logic ONLY
-	# We only run the physics engine if the boss is in the air.
-	# Once he hits the floor, we STOP calling move_and_slide().
-	# This prevents the player from pushing him.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-<<<<<<< HEAD
-		move_and_slide()
-=======
 
 	# Reset jumps when grounded
 	if is_on_floor():
@@ -71,24 +52,13 @@ func _physics_process(delta: float) -> void:
 		if is_on_floor() and not is_attacking:
 			if anim.animation != "walk":
 				anim.play("walk")
->>>>>>> 99cc24945278296f8c06a1e423dad4b6b1c984b0
 	else:
-		# Force velocity to zero just to be safe
-		velocity = Vector2.ZERO
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-# --- Combat Functions ---
-func take_damage(amount):
-	health -= amount
-	print("Boss HP: ", health)
-	if health <= 0:
-		queue_free()
+		if is_on_floor() and not is_attacking:
+			if anim.animation != "idle":
+				anim.play("idle")
 
-<<<<<<< HEAD
-func stun(time):
-	stunned = true
-	stun_timer = time
-	
-=======
 	move_and_slide()
 
 
@@ -110,8 +80,3 @@ func _on_area_2d_coin_area_entered(area: Area2D) -> void:
 func set_coin(new_coin_count: int) -> void:
 	coin_counter = new_coin_count
 	coin_label.text = "Coin Count: " + str(coin_counter )
-<<<<<<< HEAD
-	
-=======
->>>>>>> 99cc24945278296f8c06a1e423dad4b6b1c984b0
->>>>>>> d4dac0710c2aebe51322c714ed7aa45a5d5cd0ea
