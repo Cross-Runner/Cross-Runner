825f8f6d3e2add9b94581fab5b08824c7d87edb4
Gjilde <gildeoliver07@gmail.com>
2025-11-24 11:11:35 +0100

    Revert "liten mer god del"
    This reverts commit 6e64c66ee88cb73b8d7470e01e588431db739f02, reversing
    changes made to 03c7f75392f6ba6054fce51c0e6871133342a9ef.

--- cross-runner-main-gd/PlayerStats.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var max_health := 3
-var health := 3
-
-signal health_changed
--- cross-runner-main-gd/area_2d.gd
+++ cross-runner-main-gd/area_2d.gd
@@ -1,14 +1,11 @@
 extends Area2D
 
-@export var target_scene: String = "res://CR_Scene4.tscn"
+@export var target_scene: String = "res://CR_Scene2.tscn"
 
-func _ready():
-	body_entered.connect(_on_body_entered)
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
 
-func _on_body_entered(body: Node):
+func _on_body_entered(body: Node) -> void:
+	# Sjekk at det er spilleren som går inn i området
 	if body.is_in_group("player"):
-		get_node("/root/PlayerStats").health -= 1
-		get_node("/root/PlayerStats").health_changed.emit()
-
-		if get_node("/root/PlayerStats").health <= 0:
-			get_tree().change_scene_to_file(target_scene)
+		get_tree().change_scene_to_file(target_scene)
